029fa9203d50aa53fcb7b1f1c658c4e867c48c7e
komal kumari <komal135296@gmail.com>
2025-03-01 22:42:53 +0530

    chapter 3 leanred destrcutiring the data

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,109 +1,440 @@
+'use client'
 import React from 'react'
 import Restraunt from './Restraunt'
 
 function MainBody() {
 
-  const resObj=
-  {
+  const resObj=[
+    {
+      "info": {
+        "id": "41804",
+        "name": "Sweet Truth - Cake and Desserts",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/2/17/a7ba285c-35f9-4b31-9247-ea829d8a95e0_41804.jpg",
+        "locality": "Maan Road Om plaza 2nd floor",
+        "areaName": "Hinjawadi",
+        "costForTwo": "₹450 for two",
+        "cuisines": [
+          "Desserts",
+          "Bakery",
+          "Ice Cream",
+          "Snacks"
+        ],
+        "avgRating": 4.3,
+        "parentId": "4444",
+        "avgRatingString": "4.3",
+        "totalRatingsString": "3.0K+",
+        "sla": {
+          "deliveryTime": 26,
+          "lastMileTravel": 5,
+          "serviceability": "SERVICEABLE",
+          "slaString": "25-30 mins",
+          "lastMileTravelString": "5.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2025-03-01 23:59:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
               
-                "info": {
-                  "id": "182075",
-                  "name": "Pind Punjab",
-                  "cloudinaryImageId": "inuz5b1g03lxlrnck5kl",
-                  "locality": "Hinjewadi",
-                  "areaName": "Hinjawadi",
-                  "costForTwo": "₹450 for two",
-                  "cuisines": [
-                    "Indian",
-                    "Punjabi",
-                    "Chinese",
-                    "Biryani",
-                    "Thalis",
-                    "Mughlai",
-                    "Beverages",
-                    "Desserts"
-                  ],
-                  "avgRating": 4.3,
-                  "parentId": "4196",
-                  "avgRatingString": "4.3",
-                  "totalRatingsString": "26K+",
-                  "promoted": true,
-                  "adTrackingId": "cid=25577037~p=1~adgrpid=25577037#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=182075~eid=3dfa19b1-6d26-402d-ac6e-74933c9a4aec~srvts=1740415123243~collid=115117",
-                  "sla": {
-                    "deliveryTime": 39,
-                    "lastMileTravel": 4.3,
-                    "serviceability": "SERVICEABLE",
-                    "slaString": "35-40 mins",
-                    "lastMileTravelString": "4.3 km",
-                    "iconType": "ICON_TYPE_EMPTY"
-                  },
-                  "availability": {
-                    "nextCloseTime": "2025-02-24 23:30:00",
-                    "opened": true
-                  },
-                  "badges": {
-                    
-                  },
-                  "isOpen": true,
-                  "type": "F",
-                  "badgesV2": {
-                    "entityBadges": {
-                      "imageBased": {
-                        
-                      },
-                      "textBased": {
-                        
-                      },
-                      "textExtendedBadges": {
-                        
-                      }
-                    }
-                  },
-                  "aggregatedDiscountInfoV3": {
-                    "header": "60% OFF",
-                    "subHeader": "UPTO ₹120"
-                  },
-                  "loyaltyDiscoverPresentationInfo": {
-                    "logoCtx": {
-                      "text": "BENEFITS",
-                      "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
-                    },
-                    "freedelMessage": "FREE DELIVERY"
-                  },
-                  "differentiatedUi": {
-                    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-                    "differentiatedUiMediaDetails": {
-                      "lottie": {
-                        
-                      },
-                      "video": {
-                        
-                      }
-                    }
-                  },
-                  "reviewsSummary": {
-                    
-                  },
-                  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-                  "restaurantOfferPresentationInfo": {
-                    
-                  },
-                  "externalRatings": {
-                    "aggregatedRating": {
-                      "rating": "--"
-                    }
-                  },
-                  "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
-                },
-                "analytics": {
-                  "context": "seo-data-6a3416a6-dd3c-40da-98b7-78c93a502662"
-                },
-                "cta": {
-                  "link": "https://www.swiggy.com/city/pune/pind-punjab-hinjewadi-hinjawadi-rest182075",
-                  "type": "WEBLINK"
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "60% OFF",
+          "subHeader": "UPTO ₹110"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+          "logoCtx": {
+            "text": "BENEFITS",
+            "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+          },
+          "freedelMessage": "FREE DELIVERY"
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        },
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "--"
+          }
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      },
+      "analytics": {
+        "context": "seo-data-a787b390-e186-4a06-b4ad-6666f21ef890"
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/city/pune/sweet-truth-cake-and-desserts-maan-road-om-plaza-2nd-floor-hinjawadi-rest41804",
+        "text": "RESTAURANT_MENU",
+        "type": "WEBLINK"
+      },
+      "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+              
+      "info": {
+        "id": "441447",
+        "name": "Burger King",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/3/1/512cbf1e-45d4-46dc-b629-7e4b66c60107_441447.jpg",
+        "locality": "Suratwala",
+        "areaName": "Hinjewadi",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Burgers",
+          "American"
+        ],
+        "avgRating": 4.1,
+        "parentId": "166",
+        "avgRatingString": "4.1",
+        "totalRatingsString": "14K+",
+        "sla": {
+          "deliveryTime": 39,
+          "lastMileTravel": 7.7,
+          "serviceability": "SERVICEABLE",
+          "slaString": "35-40 mins",
+          "lastMileTravelString": "7.7 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2025-03-01 23:59:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "60% OFF",
+          "subHeader": "UPTO ₹120"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        },
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "4.1",
+            "ratingCount": "10K+"
+          },
+          "source": "GOOGLE",
+          "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      },
+      "analytics": {
+        "context": "seo-data-a787b390-e186-4a06-b4ad-6666f21ef890"
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/city/pune/burger-king-suratwala-hinjewadi-rest441447",
+        "type": "WEBLINK"
+      }
+                     
+    },
+    {
+      "info": {
+        "id": "661238",
+        "name": "Kwality Walls Ice Cream and More",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/13/558ecdec-88de-4d27-94fa-dfeccd817c73_661238.JPG",
+        "locality": "CRYSTAL Market",
+        "areaName": "Hinjewadi Phase-3",
+        "costForTwo": "₹200 for two",
+        "cuisines": [
+          "Desserts",
+          "Ice Cream",
+          "Ice Cream Cakes"
+        ],
+        "avgRating": 4.8,
+        "veg": true,
+        "parentId": "582",
+        "avgRatingString": "4.8",
+        "totalRatingsString": "69",
+        "sla": {
+          "deliveryTime": 26,
+          "lastMileTravel": 2,
+          "serviceability": "SERVICEABLE",
+          "slaString": "25-30 mins",
+          "lastMileTravelString": "2.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2025-03-01 22:40:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "v1695133679/badges/Pure_Veg111.png",
+              "description": "pureveg"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "pureveg",
+                    "imageId": "v1695133679/badges/Pure_Veg111.png"
+                  }
                 }
-                   
-  }
+              ]
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "40% OFF",
+          "subHeader": "UPTO ₹80"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+          "logoCtx": {
+            "text": "BENEFITS",
+            "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+          },
+          "freedelMessage": "FREE DELIVERY"
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        },
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "--"
+          }
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      },
+      "analytics": {
+        "context": "seo-data-a787b390-e186-4a06-b4ad-6666f21ef890"
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/city/pune/kwality-walls-ice-cream-and-more-crystal-market-hinjewadi-phase-3-rest661238",
+        "text": "RESTAURANT_MENU",
+        "type": "WEBLINK"
+      },
+      "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+      "info": {
+        "id": "617590",
+        "name": "Domino's Pizza",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/2/17/b0bb3e46-de8c-4eef-bc43-09f0eb712f60_617590.JPG",
+        "locality": "Phase 2",
+        "areaName": "Hinjewadi Phase-3",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+          "Pizzas",
+          "Italian",
+          "Pastas",
+          "Desserts"
+        ],
+        "avgRating": 4,
+        "parentId": "2456",
+        "avgRatingString": "4.0",
+        "totalRatingsString": "1.8K+",
+        "sla": {
+          "deliveryTime": 25,
+          "lastMileTravel": 0.2,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20-25 mins",
+          "lastMileTravelString": "0.2 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2025-03-01 23:59:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "ITEMS",
+          "subHeader": "AT ₹89"
+        },
+        "loyaltyDiscoverPresentationInfo": {
+          "logoCtx": {
+            "text": "BENEFITS",
+            "logo": "v1634558776/swiggy_one/OneLogo_3x.png"
+          },
+          "freedelMessage": "FREE DELIVERY"
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        },
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "--"
+          }
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      },
+      "analytics": {
+        "context": "seo-data-a787b390-e186-4a06-b4ad-6666f21ef890"
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/city/pune/dominos-pizza-phase-2-hinjewadi-phase-3-rest617590",
+        "text": "RESTAURANT_MENU",
+        "type": "WEBLINK"
+      },
+      "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    }
+
+  ]
+  
   return (
     <div className='mainbody'>
         <div className='search'>
@@ -112,7 +443,10 @@ function MainBody() {
         <div className='res-container'>
             {/* different restraunts list */}
             {/* here  I am assisnging the same Restraunt call just to see css how it aligns */}
-           <Restraunt resData={resObj}/> 
+           <Restraunt resData={resObj[0]}/> 
+           <Restraunt resData={resObj[1]}/> 
+           <Restraunt resData={resObj[2]}/> 
+           <Restraunt resData={resObj[3]}/> 
 
         </div>
      
--- src/components/Restraunt.jsx
+++ src/components/Restraunt.jsx
@@ -1,10 +1,15 @@
+'use client'
 import React from 'react'
 
 function Restraunt(props) {
   const {resData} =props;
 
-  console.log(resData,"resObj")
-
+  const{
+    cloudinaryImageId,
+    name,
+    cuisines,
+    avgRating
+  } = resData?.info  // destructuring the data is always a good practice to make our code optimize
     const stylecard={
         backgroundColor: "#f0f0f0",
     };
@@ -13,10 +18,10 @@ function Restraunt(props) {
         <img
         className='res-logo'
          alt="restrauntlogo" 
-         src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_324,c_fill/zvqgvwxrxsemvouvqvg3"/>
-        <h3>{resData.info.name}</h3>
-        <h4> {props.cuisines}</h4>
-        <h5>Ratings 5 star</h5>
+         src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}/>
+        <h3>{name}</h3>
+        <h4> {cuisines.join(",")}</h4>
+        <h5>{avgRating}star</h5>
         <h6>30 minutes</h6>
     </div>
   )
