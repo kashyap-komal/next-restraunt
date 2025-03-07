4e379cfa080b1aba493a910ff309bef315601ac1
komal kumari <komal135296@gmail.com>
2025-03-07 22:37:06 +0530

    fix cors plugin issue uisng cors proxy

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -16,7 +16,7 @@ function MainBody() {
 
   const fetchData= async()=>{
     const data=await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5865395&lng=73.6986675&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5865395&lng=73.6986675&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     )
 
     const josn=await data.json();
