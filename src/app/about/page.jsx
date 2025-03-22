affd44a1e4fe6b94f76dd4f434b0cd405ffdc672
komal kumari <komal135296@gmail.com>
2025-03-22 23:26:10 +0530

    learned about class based func lec-8

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,22 @@
+'use client'
+import React,{ useEffect } from "react";
+
 export default function AboutPage() {
+
+  useEffect(()=>{
+    const timer=setInterval(()=>{
+      console.log("namaste react")
+    },1000);
+    return ()=>{
+      clearInterval(timer);
+      console.log("remove timer equal to unmount")
+    }
+  },[])
     return (
       <div className="about-container">
         <h1>About Us</h1>
         <p>Welcome to our restaurant! We are committed to serving the best food with love and care.</p>
+        <p>This project is made with nextjs for learning purpose with the help of namaste react</p>
       </div>
     );
   }
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Contactpage() {
   return (
     <div>
-      I am in contact
+     <h1>This is Contact us Page</h1>
+    <h2>Name : Komal</h2>
+    <h3>Email us at: kmal456@gmail.com</h3>
     </div>
   )
 }
